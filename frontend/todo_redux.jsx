405c8879eb394741f6a8761e30957bf2b5ee1e28
Edward Bai <edwardbai31@gmail.com>
2017-11-13 13:25:11 -0800

    Initial commit; setup weback envir

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+document.addEventListener('DOMContentLoaded', () =>{
+  const roots = document.getElementById('root');
+  ReactDom.render(<h1>TODOs APP</h1>, roots);
+});
